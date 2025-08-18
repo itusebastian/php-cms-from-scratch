9f53ed799d5f941fbce68faa28bb987f84fd717d
Sebastian Iturralde <itusebastian@gmail.com>
2025-08-18 10:08:55 -0500

    Add URL helper function

--- app/public/staff/index.php
+++ app/public/staff/index.php
@@ -4,6 +4,13 @@
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
+  <div id="main-menu">
+    <h2>Main Menu</h2>
+    <ul>
+      <li><a href="<?php echo url_for('/staff/subjects'); ?>">Subject List</a></li>
+      <li><a href="<?php echo url_for('/staff/subjects/new'); ?>">Add Subject</a></li>
+    </ul>
+  </div>
 </div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
