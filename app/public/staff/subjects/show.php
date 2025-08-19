dea344459f7bfc6eac61964d045fa82cfe136021
Sebastian Iturralde <itusebastian@gmail.com>
2025-08-19 08:24:59 -0500

    Refactor URL encoding functions and update show page to utilize them

--- app/public/staff/subjects/show.php
+++ app/public/staff/subjects/show.php
@@ -1,5 +1,11 @@
-<?php
+<?php require_once('../../../private/initialize.php');
 
 $id = $_GET['id'] ?? '1'; // Default to 1 if no ID is provided
 
 echo $id;
+
+?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a>
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a>
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a>
