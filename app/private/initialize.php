17ae146f537b9422cea1958300e6c0ca980294e8
Sebastian Iturralde <itusebastian@gmail.com>
2025-08-21 08:20:55 -0500

    Add output buffering to initialize.php

--- app/private/initialize.php
+++ app/private/initialize.php
@@ -1,5 +1,8 @@
 <?php
 
+ob_start(); // output buffering is turned on
+
+
 // Assign file paths to PHP constants
 // _FILE_ returns the current file path
 // dirname() returns the parent directory path
