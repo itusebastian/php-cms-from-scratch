db3125ba0adce0ed93bd9f72fa5749d19a0dfcc8
Sebastian Iturralde <itusebastian@gmail.com>
2025-08-24 18:20:35 -0500

    Refactor edit.php to handle form submissions and improve visibility of form values

--- app/public/staff/subjects/edit.php
+++ app/public/staff/subjects/edit.php
@@ -2,15 +2,28 @@
 
 require_once('../../../private/initialize.php');
 
-$test = $_GET['test'] ?? '';
-
-if ($test == '404') {
-  error_404();
-} elseif ($test == '500') {
-  error_500();
-} elseif ($test == 'redirect') {
-  redirect_to('/staff/subjects/index.php');
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/staff/subjects/index.php'));
 }
+$id = $_GET['id'];
+$menu_name = '';
+$position = '';
+$visible = '';
+
+if(is_post_request()) {
+
+  // Handle form values sent by new.php
+
+  $menu_name = $_POST['menu_name'] ?? '';
+  $position = $_POST['position'] ?? '';
+  $visible = $_POST['visible'] ?? '';
+
+  echo "Form parameters<br />";
+  echo "Menu name: " . $menu_name . "<br />";
+  echo "Position: " . $position . "<br />";
+  echo "Visible: " . $visible . "<br />";
+}
+
 ?>
 
 <?php $page_title = 'Edit Subject'; ?>
@@ -23,16 +36,16 @@ if ($test == '404') {
   <div class="subject edit">
     <h1>Edit Subject</h1>
 
-    <form action="" method="post">
+    <form action="<?php echo url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
-        <dd><input type="text" name="menu_name" value="" /></dd>
+        <dd><input type="text" name="menu_name" value="<?php echo h($menu_name); ?>" /></dd>
       </dl>
       <dl>
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1">1</option>
+            <option value="1"<?php if($position == "1") { echo " selected"; } ?>>1</option>
           </select>
         </dd>
       </dl>
@@ -40,11 +53,11 @@ if ($test == '404') {
         <dt>Visible</dt>
         <dd>
           <input type="hidden" name="visible" value="0" />
-          <input type="checkbox" name="visible" value="1" />
+          <input type="checkbox" name="visible" value="1"<?php if($visible == "1") { echo " checked"; } ?> />
         </dd>
       </dl>
       <div id="operations">
-        <input type="submit" value="Create Subject" />
+        <input type="submit" value="Edit Subject" />
       </div>
     </form>
 
