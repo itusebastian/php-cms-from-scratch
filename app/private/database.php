f46b3de94dad23fc6e074ff52200ccb28d408347
Sebastian Iturralde <itusebastian@gmail.com>
2025-08-26 08:51:07 -0500

    Fix require statement to use db_credentials.php and ensure connection confirmation is called in db_connect function

--- app/private/database.php
+++ app/private/database.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once 'db_connection.php';
+require_once 'db_credentials.php';
 
 function db_connect()
 {
   $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+  confirm_db_connect();
   return $connection;
 }
 
@@ -12,5 +13,15 @@ function db_disconnect($connection)
 {
   if (isset($connection)) {
     mysqli_close($connection);
+  }
+}
+
+function confirm_db_connect()
+{
+  if (mysqli_connect_errno()) {
+    $msg = "Database connection failed: ";
+    $msg .= mysqli_connect_error();
+    $msg .= " (" . mysqli_connect_errno() . ")";
+    exit($msg);
   }
 }
