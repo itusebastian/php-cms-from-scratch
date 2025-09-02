0dcb9e695ec8624b2882a5bae8ebaa916fffdf83
Sebastian Iturralde <itusebastian@gmail.com>
2025-09-02 08:50:39 -0500

    Refactor insert_subject and update_subject functions to accept a subject associative array for improved consistency and maintainability

--- app/private/query_functions.php
+++ app/private/query_functions.php
@@ -24,16 +24,16 @@ function find_subject_by_id($id)
   return $subject;
 }
 
-function insert_subject($menu_name, $position, $visible)
+function insert_subject($subject)
 {
   global $db;
 
   $sql = "INSERT INTO subjects ";
   $sql .= "(menu_name, position, visible) ";
   $sql .= "VALUES (";
-  $sql .= "'" . $menu_name . "', ";
-  $sql .= "'" . $position . "', ";
-  $sql .= "'" . $visible . "'";
+  $sql .= "'" . $subject['menu_name'] . "', ";
+  $sql .= "'" . $subject['position'] . "', ";
+  $sql .= "'" . $subject['visible'] . "'";
   $sql .= ")";
   $result = mysqli_query($db, $sql);
   // For INSERT statements, $result is true/false
@@ -47,6 +47,29 @@ function insert_subject($menu_name, $position, $visible)
   }
 }
 
+function update_subject($subject)
+{
+  global $db;
+
+  $sql = "UPDATE subjects SET ";
+  $sql .= "menu_name='" . $subject['menu_name'] . "', ";
+  $sql .= "position='" . $subject['position'] . "', ";
+  $sql .= "visible='" . $subject['visible'] . "' ";
+  $sql .= "WHERE id='" . $subject['id'] . "' ";
+  $sql .= "LIMIT 1";
+
+  $result = mysqli_query($db, $sql);
+  // For UPDATE statements, $result will be true/false
+  if ($result) {
+    // Success
+    return true;
+  } else {
+    // Failure
+    echo mysqli_error($db);
+    db_disconnect($db);
+    exit;
+  }
+}
 function find_all_pages()
 {
   global $db;
