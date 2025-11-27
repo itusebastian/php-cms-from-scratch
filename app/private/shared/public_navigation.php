471cac8ed4eaa198ff0ea1d9f718f90b29856672
Sebastian Iturralde <itusebastian@gmail.com>
2025-11-27 08:07:07 -0500

    Fix navigation highlighting by setting subject_id in index.php and updating list item classes in public_navigation.php

--- app/private/shared/public_navigation.php
+++ app/private/shared/public_navigation.php
@@ -2,14 +2,14 @@
   <?php $nav_subjects = find_all_subjects(); ?>
   <ul class="subjects">
     <?php while ($nav_subject = mysqli_fetch_assoc($nav_subjects)) { ?>
-      <li>
+      <li class="<?php echo $nav_subject['id'] == $subject_id ? 'selected' : ''; ?>">
         <a href="<?php echo url_for('index.php'); ?>">
           <?php echo h($nav_subject['menu_name']); ?>
         </a>
         <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
         <ul class="pages">
           <?php while ($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
-            <li>
+            <li class="<?php echo $nav_page['id'] == $page_id ? 'selected' : ''; ?>">
               <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
                 <?php echo h($nav_page['menu_name']); ?>
               </a>
